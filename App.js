c25d630a05be5b1dfdd9bfe8775e7342c78380c4
Adrian Mostowski <adaxelx@o2.pl>
2019-08-22 10:44:58 +0200

    initial config

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styled from 'styled-components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <StyledView>
+      <Text>siema Dzusia</Text>
+    </StyledView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const StyledView = styled(View)`
+  flex: 1;
+  background-color: blue;
+  align-items: center;
+  justify-content: center;
+`;
