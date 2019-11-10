92956a85d213800f9b8b755e3c253caf5bc813b1
Adrian Mostowski <adaxelx@o2.pl>
2019-11-10 12:33:17 +0100

    add basics of all records

--- views/Songs/AllRecords/index.js
+++ views/Songs/AllRecords/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+// import RecItem from 'rap-gra/views/Songs/RecItem';
 import AllItems from 'rap-gra/templates/AllItemsTemplate';
 
 const AllRecords = () => {
+  // const data = records.map();
   return <AllItems title="Wszystkie płyty" />;
 };
 
